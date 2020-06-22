65c5f65898acad94fddac0874d2ef0751b9cdfec
Anil Dhurjaty <adhurjaty@gmail.com>
2020-06-22 09:03:45 -0700

    enables https on node server

--- view/app.js
+++ view/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
 const proxy = require('http-proxy').createProxyServer({
     host: 'http://api:5000'
@@ -8,6 +11,10 @@ const proxy = require('http-proxy').createProxyServer({
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+
+var privateKey = fs.readFileSync('secrets/node-selfsigned.key', 'utf-8');
+var cert = fs.readFileSync('secrets/node-selfsigned.crt', 'utf-8');
+var creds = {key: privateKey, cert: cert};
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -21,4 +28,7 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 3000, () => console.log(`Running on PORT ${process.env.PORT || 3000}`));
+var httpsServer = https.createServer(creds, app)
+httpsServer.listen(3000);
+console.log('Running on PORT 3000');
+// app.listen(process.env.PORT || 3000, () => console.log(`Running on PORT ${process.env.PORT || 3000}`));
