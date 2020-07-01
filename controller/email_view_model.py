8fc68c8ff89f214dc400fdc046ea49602d2f0c1d
Anil Dhurjaty <adhurjaty@gmail.com>
2020-07-01 13:47:23 -0700

    adds email vm

---
+++ controller/email_view_model.py
@@ -0,0 +1,9 @@
+from interactor.account_manager_response import AccountManagerResponse
+
+class EmailViewModel:
+    def signup_email(self, user: AccountManagerResponse):
+        pass
+
+    def delete_user_email(self, user: AccountManagerResponse):
+        pass
+
