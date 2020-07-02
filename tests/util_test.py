34d859abf071cc4b7679098668940d944b240af5
Anil Dhurjaty <adhurjaty@gmail.com>
2020-07-02 11:13:33 -0700

    tests logger

--- tests/util_test.py
+++ tests/util_test.py
@@ -1,4 +1,6 @@
+from .mocks import MockObject
 import util.utils as utils
+import util.app_logger as al
 
 
 def test_sign_verify_token():
@@ -7,3 +9,32 @@ def test_sign_verify_token():
     signed = utils.get_signed_token(token)
     assert utils.verify_token(token, signed)
     assert not utils.verify_token('token', signed)
+
+
+def test_logger_correct_type():
+    assert isinstance(al.logger, al.AppLogger)
+
+
+def test_logger_error_send_email():
+    validations = MockObject()
+    validations.contents = {}
+
+    def send_email(contents):
+        validations.contents = contents
+
+    al.config = {
+        'EMAIL_USERNAME': 'foo@example.com',
+        'EMAIL_PASSWORD': 'LKASDJFLK'
+    }
+
+    logger = al.logger
+    logger.emailer = MockObject()
+    logger.emailer.send_email = send_email
+    
+    logger.error('this error', 'my stack')
+    
+    assert validations.contents == {
+        'to': 'foo@example.com',
+        'subject': 'PROCORE CALENDAR APP ERROR',
+        'contents': f'message: this error\nstack trace: my stack'
+    }
